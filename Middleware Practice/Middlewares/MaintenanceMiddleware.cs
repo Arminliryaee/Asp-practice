3307b6e2278e6edbf592b2b8a8f2016d05fc606b
armin <liryaeearmin50@gmail.com>
2025-08-17 14:14:54 +0330

    MaintenanceMiddleware logic is Updated.

--- Middleware Practice/Middlewares/MaintenanceMiddleware.cs
+++ Middleware Practice/Middlewares/MaintenanceMiddleware.cs
@@ -12,7 +12,7 @@ public class MaintenanceMiddleware
     public async Task InvokeAsync(HttpContext ctx)
     {
         string Path = ctx.Request.Path.Value.ToLower();
-        if (Path.Contains("maintenance"))
+        if (Path.Contains("maintenance") || Path.Contains("home"))
         {
             ctx.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
             await ctx.Response.WriteAsync("<h1>Site is under maintenance. Please check back later.</h1>");
