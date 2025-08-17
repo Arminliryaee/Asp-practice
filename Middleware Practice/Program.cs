8f12128f93dc1562e37993ff372bdf259addf590
armin <liryaeearmin50@gmail.com>
2025-08-17 11:13:50 +0330

    Add project files.

---
+++ Middleware Practice/Program.cs
@@ -0,0 +1,47 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+app.Use(async (ctx, next) =>
+{
+    ctx.Response.ContentType = "text/html; charset=utf-8";
+    string PathValue = ctx.Request.Path.Value;
+    if (PathValue.Contains("Debug"))
+    {
+        await next(ctx);
+        await ctx.Response.WriteAsync("<div style='background-color: lightblue; border: 1px solid blue; padding: 10px;'>Debug mode is active.</div>\n");
+    }
+    else
+        await next(ctx);
+});
+
+app.Use(async (ctx, next) =>
+{
+    string? PathValue = ctx.Request.Path.Value;
+    if (PathValue is not null && PathValue.StartsWith("/admin/"))
+    {
+        await next(ctx);
+        await ctx.Response.WriteAsync("<div style='background-color: yellow; text-align: center; padding: 5px;'>Attention: You are in the admin section.</div>\n");
+    }
+    else
+        await next(ctx);
+});
+
+app.Use(async (ctx, next) =>
+{
+    DateTime DateNow = DateTime.Now;
+    await next(ctx);
+    await ctx.Response.WriteAsync(DateNow.Hour switch
+    {
+        < 12 => "Good morning!\n",
+        < 18 => "Good afternoon!\n",
+        _ => "Good evening!\n"
+    });
+});
+
+
+
+app.MapGet("/Home", () => "Nothing to show yet.");
+
+
+app.Run();
